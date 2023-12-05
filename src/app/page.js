875177a60eda50d0db4494744ea64dd14659e449
Samy6Almeida <50253688+Samy6Almeida@users.noreply.github.com>
2023-12-05 13:17:26 -0300

    Test - noverify

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-
+'use client';
+import { Button } from '@carbon/react';
 export default function Home() {
   return (
     <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+      <text>
+        Hello Carbon! Well, not quite yet. This is the starting point for the
+        Carbon NextJS tutorial.
+      </text>
+      <br />
+      <Button>Button</Button>
     </div>
-
-  )
+  );
 }
