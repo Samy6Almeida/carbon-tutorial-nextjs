df0d090de48e19c812f2291221e2f4032df66317
Samy6Almeida <50253688+Samy6Almeida@users.noreply.github.com>
2023-12-07 14:18:19 -0300

    feat(tutorial): complete step 4

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import LandingPage from './home/page';
+import { InfoSection, InfoCard } from '@/components/Info/Info';
 
 export default function Page() {
-    return <LandingPage />;
-  }
+  return <LandingPage />;
+}
